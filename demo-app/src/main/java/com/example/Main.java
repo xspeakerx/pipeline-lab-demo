67b61a00ea29ad2af4a062d512c7c0c86c386d02
Nikolay <53034686+xspeakerx@users.noreply.github.com>
2026-06-21 17:49:33 +0500

    Update Main.java

--- demo-app/src/main/java/com/example/Main.java
+++ demo-app/src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello from pipeline lab!");
+        System.out.println("Hello from pipeline and trigger lab!");
         Thread.sleep(Long.MAX_VALUE); // чтобы под не падал сразу
     }
 }
