f524f61c73a9af332467bf1b74aaddc0e43157bc
Nikolay <nzverev@nzverev.lab>
2026-06-21 09:01:38 +0500

    initial app

---
+++ apps/demo-app/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello from pipeline lab!");
+        Thread.sleep(Long.MAX_VALUE); // чтобы под не падал сразу
+    }
+}
